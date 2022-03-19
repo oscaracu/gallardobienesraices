a2b41bc3d35cac3dc48424d80fc61c9857af8f84
Angel Cu <oscaracu@miplazaweb.com.mx>
2022-03-19 00:42:51 -0600

    SimpleParallax added

--- src/template.js
+++ src/template.js
@@ -1,42 +1,51 @@
 
 // Splide
 
-document.addEventListener( 'DOMContentLoaded', function () {
-    var main = new Splide( '#main-slider', {
-      type      : 'fade',
-      rewind    : true,
-      pagination: false,
-      arrows    : true,
-    } );
-  
-  
-    var thumbnails = new Splide( '#thumbnail-slider', {
-      fixedWidth  : 113,
-      fixedHeight : 75,
-      gap         : 10,
-      rewind      : true,
-      pagination  : false,
-      cover       : true,
-      isNavigation: true,
-      arrows      : false,
-      focus       : 'center',
-      breakpoints : {
-        600: {
-          fixedWidth : 75,
-          fixedHeight: 50,
-        },
-      },
-    } );
-  
-  
-    main.sync( thumbnails );
-    main.mount();
-    thumbnails.mount();
-  } );
-
-  // Active NavLinks
-
-  let itemsActive = document.querySelectorAll('header nav li.active a');
-  itemsActive.forEach(navLink => {
-      navLink.classList.add("active");
+document.addEventListener('DOMContentLoaded', function () {
+  var main = new Splide('#main-slider', {
+    type: 'fade',
+    rewind: true,
+    pagination: false,
+    arrows: true,
   });
+
+
+  var thumbnails = new Splide('#thumbnail-slider', {
+    fixedWidth: 113,
+    fixedHeight: 75,
+    gap: 10,
+    rewind: true,
+    pagination: false,
+    cover: true,
+    isNavigation: true,
+    arrows: false,
+    focus: 'center',
+    breakpoints: {
+      600: {
+        fixedWidth: 75,
+        fixedHeight: 50,
+      },
+    },
+  });
+
+
+  main.sync(thumbnails);
+  main.mount();
+  thumbnails.mount();
+});
+
+// Active NavLinks
+
+let itemsActive = document.querySelectorAll('header nav li.active a');
+itemsActive.forEach(navLink => {
+  navLink.classList.add("active");
+});
+
+
+// simpleParallax
+
+var image = document.getElementsByClassName('cover-image');
+new simpleParallax(image, {
+  delay: .6,
+  transition: 'cubic-bezier(0,0,0,1)'
+});
