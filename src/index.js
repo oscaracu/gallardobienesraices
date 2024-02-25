e5cf10c609b81f3e0e5b89ade4174c690108e77b
Oscar del Angel Cu Reyes <oscaracu@miplazaweb.com.mx>
2024-02-24 21:07:47 -0600

    rev240224

--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
-import * as bootstrap from 'bootstrap';
-import SimpleParallax from 'simple-parallax-js';
-import '@fortawesome/fontawesome-free/css/all.css';
-
-// Active NavLinks
-
-let itemsActive = document.querySelectorAll('header nav li.active a');
-itemsActive.forEach(navLink => {
-  navLink.classList.add("active");
-});
-
+import * as bootstrap from "bootstrap";
+import SimpleParallax from "simple-parallax-js";
+import "@fortawesome/fontawesome-free/css/all.css";
 
 // simpleParallax
 
-var image = document.getElementsByClassName('cover-image');
+var image = document.getElementsByClassName("cover-image");
 new SimpleParallax(image, {
-  delay: .6,
-  transition: 'cubic-bezier(0,0,0,1)'
+  delay: 0.6,
+  transition: "cubic-bezier(0,0,0,1)",
 });
